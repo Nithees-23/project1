284e984283781f78f189d8aee6bd0f7c6e9b90fa
Nithees-23 <120078034+Nithees-23@users.noreply.github.com>
2022-12-12 16:14:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import List from './List1';
+import './App.css';
+import Radium1 from './Radium';
+import BasicButtons from './button';
+function App() {
+  return (
+    <div className="App">
+      <List />
+      <Radium1 />
+      <BasicButtons />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Nav.js
@@ -0,0 +1,10 @@
+import { AppBar } from "@mui/material";
+import { render } from "@testing-library/react";
+import React from "react";
+export default class Header AppBar extends React.component{
+    render(){
+        <>
+        
+        </>
+    }
+}
